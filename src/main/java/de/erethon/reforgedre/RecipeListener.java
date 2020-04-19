6fe529089db79653b5a7f215c9328b9c016a8487
Malfrador <malfrador@gmail.com>
2020-04-19 19:32:34 +0200

    Enabled axes, replaced them with 0 damage versions

--- src/main/java/de/erethon/reforgedre/RecipeListener.java
+++ src/main/java/de/erethon/reforgedre/RecipeListener.java
@@ -18,12 +18,23 @@ package de.erethon.reforgedre;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import static org.bukkit.Material.*;
+import static org.bukkit.Material.WOODEN_AXE;
 
 /**
  * @author Daniel Saukel
@@ -40,6 +51,18 @@ public class RecipeListener implements Listener {
     public void onCraftItem(CraftItemEvent event) {
         ItemStack result = event.getInventory().getResult();
         if (plugin.disabledRecipes.contains(result.getType())) {
+            if ((result.getType() == Material.STONE_AXE) || result.getType() == Material.WOODEN_AXE || result.getType() == IRON_AXE || result.getType() == GOLDEN_AXE || result.getType() == DIAMOND_AXE) {
+                AttributeModifier mod = new AttributeModifier(UUID.randomUUID(), "reforgedre.disabled", -1, AttributeModifier.Operation.ADD_SCALAR);
+                ItemMeta meta = result.getItemMeta();
+                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, mod);
+                meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+                List<String> lore = new ArrayList<>();
+                lore.add(ChatColor.DARK_PURPLE + "Werkzeug");
+                lore.add("" + ChatColor.GRAY + ChatColor.ITALIC + "Verursacht keinen Schaden");
+                meta.setLore(lore);
+                result.setItemMeta(meta);
+                return;
+            }
             ItemMeta meta = result.getItemMeta();
             List<String> lore = new ArrayList<>();
             lore.add(ChatColor.DARK_RED + "Aufwendigere Schwerter und R\u00fcstungen");
